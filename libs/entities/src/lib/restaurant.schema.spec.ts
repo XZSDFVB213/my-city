1c95f98fc00a0ac1d1854d7aa632669b2beddb47
XZSDFVB213 <abakarmadatov2@gmail.com>
2026-01-15 20:11:32 +0300

    tsconfig6

--- libs/entities/src/lib/restaurant.schema.spec.ts
+++ libs/entities/src/lib/restaurant.schema.spec.ts
@@ -1,5 +1,5 @@
 import { RestaurantEntity } from './restaurant.schema';
-
+import '@types/jest';
 describe('RestaurantEntity', () => {
   it('should work', () => {
     expect(new RestaurantEntity()).toEqual('RestaurantEntity');
